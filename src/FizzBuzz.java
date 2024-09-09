c8191e773b83777cb7cd0b5b3665d7771a149696
Jinny Yoo <hyejin.yoo@mail.utoronto.ca>
2024-09-09 16:31:27 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,12 +8,12 @@ class FizzBuzz {
 
         int i = 1;
         while (i < 100) {
-            // Find out which numbers divide i.
             i = doFizzBuzz(i);
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
+        // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
